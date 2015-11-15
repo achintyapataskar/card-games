5a00ff58cf44a2375abe8f8f18bf3ac7130a987d
Achintya Pataskar <achintyapataskar@Achintyas-MacBook-Pro.local>
2015-11-15 17:51:56 +0530

    Another submission

--- blackjack_new/actions.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "blackjack.h"
-void hit(hand *player) {
-	
--- blackjack_new/images.c
+++
@@ -1,6 +0,0 @@
-#include <gtk/gtk.h>
-GtkWidget *get_image(card *shoe, int cno) {
-	int i = 0;
-	GtkWidget
-	while(i < cno) {
-		
